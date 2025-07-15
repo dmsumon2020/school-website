cd89f9ba9ee7f91565f0e396a89f982cd6a70a26
dmsumon2020 <dmsumon2020@gmail.com>
2025-07-16 01:36:32 +0600

    fixed quote issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import HeroSection from "@/components/HeroSection";
 import HeadmasterMessageSection from "@/components/Homepage/HeadmasterMessageSection";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-
       <HeadmasterMessageSection />
     </>
   );
