4b9e6c3e0dbf8af2eea96bcdce9979739848bcd2
dmsumon2020 <dmsumon2020@gmail.com>
2025-07-18 02:03:16 +0600

    added logo on navbar2

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <header className="bg-white shadow-md sticky top-0 z-50">
       <nav className="container mx-auto flex justify-between items-center px-4 py-6">
-        <div className="text-xl font-bold flex flex-row items-center gap-4">
+        <div className="text-xl font-bold flex flex-row items-center gap-4 text-theme-ascent">
           <Image
             src="/images/logo.jpg"
             alt="School Logo"
